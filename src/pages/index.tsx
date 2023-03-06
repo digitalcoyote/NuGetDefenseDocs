c5e3e27aabb890668fc930b9ec4d6b53e860f36b
digitalcoyote <lokiphasma@gmail.com>
2023-03-05 21:21:11 -0600

    Added Documentation for NugetDefense.json

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -29,8 +29,7 @@ export default function Home(): JSX.Element {
   const {siteConfig} = useDocusaurusContext();
   return (
     <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      title={`HOfficial Documentation for  ${siteConfig.title}`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
